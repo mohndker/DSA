dc371fbfee87556c47edaf14b20988855d7cb538
mohk1993 <mohanad.kh33@gmail.com>
2024-05-26 14:42:43 +0300

    merge buttom up implementation

---
+++ src/Sort/MergeBU.java
@@ -0,0 +1,60 @@
+package Sort;
+
+import java.util.Scanner;
+
+public class MergeBU {
+
+    private MergeBU() { }
+
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void sort(Comparable[] a) {
+        int n = a.length;
+        Comparable[] aux = new Comparable[n];
+        for (int len = 1; len < n; len *= 2) {
+            for (int lo = 0; lo < n - len; lo += 2 * len) {
+                int mid = lo + len - 1;
+                int hi = Math.min(lo + (2 * len) - 1, n - 1);
+                merge(a, aux, lo, mid, hi);
+            }
+        }
+        assert isSorted(a);
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] a = input.split(" ");
+        MergeBU.sort(a);
+        MergeBU.show(a);
+    }
+}
