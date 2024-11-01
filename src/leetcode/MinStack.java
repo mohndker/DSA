98f24d783de1d2ac24af8ac0f54121f3578819fb
mohk1993 <mohanad.kh33@gmail.com>
2024-11-01 18:19:10 +0200

    min stack

---
+++ src/leetcode/MinStack.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class MinStack {
+    private Node first;
+
+    public MinStack() { };
+
+    public void push(int val) {
+        if (first == null)
+            first = new Node(val, val, null);
+        else first = new Node(val, Math.min(val, first.min), first);
+    }
+
+    public void pop() {
+        first = first.next;
+    }
+
+    public int top() {
+        return first.item;
+    }
+
+    public int getMin() {
+        return first.min;
+    }
+
+    private static class Node {
+        private int item;
+        private int min;
+        private Node next;
+
+        public Node(int val, int min, Node nex) {
+            this.min = min;
+            this.item = val;
+            this.next = nex;
+        }
+    }
+}
