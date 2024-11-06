1cb2dcf5a6c3d47ce7dc61b454b79f1b8c01362e
mohk1993 <mohanad.kh33@gmail.com>
2024-11-06 11:40:21 +0200

    group anagram

---
+++ src/leetcode/hashmap/GroupAnagrams.java
@@ -0,0 +1,36 @@
+package leetcode.hashmap;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {
+                "eat",
+                "tea",
+                "tan",
+                "ate",
+                "nat",
+                "bat"
+        };
+
+        System.out.println(groupAnagrams(strs));
+    }
+
+    private static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord))
+                map.put(sortedWord, new ArrayList<>());
+
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+}
