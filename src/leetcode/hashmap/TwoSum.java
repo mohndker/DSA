dcad17ada883676684c8f518eea756250d87d950
mohk1993 <mohanad.kh33@gmail.com>
2024-11-06 12:16:04 +0200

    two sum

---
+++ src/leetcode/hashmap/TwoSum.java
@@ -0,0 +1,25 @@
+package leetcode.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement) && map.get(complement) != i)
+                return new int[] {map.get(complement), i};
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] { };
+    }
+}
