f5a25cb01fae3ed9b6a42cbbacf8a2a4ce407153
mohk1993 <mohanad.kh33@gmail.com>
2024-11-08 19:29:40 +0200

    copy random list

---
+++ src/leetcode/linkedlist/CopyListWithRandomPointer.java
@@ -0,0 +1,44 @@
+package leetcode.linkedlist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyListWithRandomPointer {
+    private static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private static Node copyRandomNode(Node head) {
+        if (head == null) return head;
+
+        Map<Node, Node> map = new HashMap<>();
+        Node curr = head;
+
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+
+        curr = head;
+
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
+}
