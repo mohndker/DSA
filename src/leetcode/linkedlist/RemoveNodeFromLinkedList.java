5e4f7880f9148bff506b1b896f36513d9fb4460e
mohk1993 <mohanad.kh33@gmail.com>
2024-11-10 21:57:30 +0200

    remove nth node from linked list

---
+++ src/leetcode/linkedlist/RemoveNodeFromLinkedList.java
@@ -0,0 +1,42 @@
+package leetcode.linkedlist;
+
+public class RemoveNodeFromLinkedList {
+    private static class ListNode {
+        ListNode next;
+    }
+
+    private static ListNode removeNode(ListNode head, int n) {
+        // [1, 2, 3, 4, 5, 6, 7] n = 3
+        // [1, 2, 3] n = 3
+        // [1] n = 1
+
+        ListNode first = head;
+        int count = 0;
+        while (first != null) {
+            count++;
+            first = first.next;
+        }
+
+        first = head;
+
+        if (count == n) {
+            first = head.next;
+            head.next = null;   // avoid loitering
+            return first;
+        }
+
+        for (int i = 1; i < count - n; i++) {
+            first = first.next;
+        }
+
+        ListNode del = first.next;
+        first.next = del.next;
+        del.next = null;    //avoid loitering
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
