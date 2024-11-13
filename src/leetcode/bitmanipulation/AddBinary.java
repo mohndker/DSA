e2f30cddc40eb0e3e0a539061f33c559d9bd9a08
mohk1993 <mohanad.kh33@gmail.com>
2024-11-13 22:40:03 +0200

    add binary

---
+++ src/leetcode/bitmanipulation/AddBinary.java
@@ -0,0 +1,28 @@
+package leetcode.bitmanipulation;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+        String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+        System.out.println(addBinary(a, b));
+    }
+
+    private static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            sb.append(sum % 2);
+        }
+        if (carry != 0) sb.append(carry);
+
+        return sb.reverse().toString();
+    }
+}
